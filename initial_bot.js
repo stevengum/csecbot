9b5ea532457583040acf2266bc53a20ffab5b3d2
Steven Gum <steven.gum@gmail.com>
2017-01-27 18:03:09 -0800

    incorporated light contact info statements into BuyItem, established regex for email in ContactInfo

--- initial_bot.js
+++ initial_bot.js
@@ -35,7 +35,7 @@ dialog.matches('BuyItem',
 
 bot.dialog('/BuyItem', [
     function (session, results, next) {
-        console.log(results.entities);
+        // console.log(results.entities);
         let product = builder.EntityRecognizer.findEntity(results.entities, 'order.product.name');
         let quantity = builder.EntityRecognizer.findEntity(results.entities, 'order.product.quantity');
         if(!quantity) {
@@ -59,6 +59,7 @@ bot.dialog('/BuyItem', [
         if(results.response && !order.product) {
             order.product = results.response;
         }
+
         if(!order.quantity) {
             builder.Prompts.number(session, 'How many would you like to order?');
         } else {
@@ -70,6 +71,13 @@ bot.dialog('/BuyItem', [
         let order = session.dialogData.order;
         if(typeof results.response == "number") {
             order.quantity = results.response;
+            // currently do not remember what this little bit is for.
+            // let temp = results.entities ? results.entities : "No results.entities";
+            // let temp = builder.EntityRecognizer.findEntity(results.entities, 'builtin.number');
+            // console.log(temp);
+            // if(typeof temp == "string"){
+            //     console.log("here is the results object:\n", results);
+            // }
         }
 
         if(!order.quantity) {
@@ -81,42 +89,144 @@ bot.dialog('/BuyItem', [
     //end third BuyItem function
     function (session, results, next) {
         let order = session.dialogData.order;
+        if(typeof results.response == "number") {
+            order.quantity = results.response;
+        }
+
         if(order.product && order.quantity) {
-            builder.Prompts.text(session, `If I understand correctly, you wish to order ${order.quantity} ${order.product}? yes/no`);
+            builder.Prompts.confirm(session, `If I understand correctly, you wish to order ${order.quantity} "${order.product}"? yes/no`);
         } else {
             next();
         }
     },
     //end fourth BuyItem function
+
     function (session, results, next) {
         let order = session.dialogData.order;
-        let user = session.userData
-        if(user.name || user.email || user.phone) {
-            console.log(session.userData);
-        };
-        if(results.response == "yes") {
-            if(!session.userData) {
+        let user = session.userData;
 
+        if(results.response) {
+            if(!user.email && !user.phone) {
+                //if no contact info, yes will 'beginDialog('/ContactInfo')'
+                builder.Prompts.confirm(session, "You replied \"yes\", unfortunately your contact information is not on hand, if you wish to do so, we are still able to complete your order.\nIf you'd like to, you may submit your contact information. Would you like to do so? yes/no");
+            } else if (!user.email){
+                //if no email, yes will start builder.Prompts.text(session, "Okay, what is your email address?");
+                builder.Prompts.confirm(session, "You replied \"yes\", unfortunately your email is not on hand, if you wish to do so, we are still able to complete your order.\nIf you'd like to, you may submit your email. Would you like to do so? yes/no");
+            } else if (!user.phone){
+                //if no email, yes will start builder.Prompts.text(session, "Okay, what is your phone number?");
+                builder.Prompts.confirm(session, "You replied \"yes\", unfortunately your phone number is not on hand, if you wish to do so, we are still able to complete your order.\nIf you'd like to, you may submit your phone number. Would you like to do so? yes/no");
+
+            } else {
+                //if both user.email && user.phone, confirm order and close dialog.
+                session.endDialog(`You said "yes", order confirmed! This dialog will now end; thanks for ordering!`);
             }
-            session.endDialog(`You said "yes", order confirmed! This dialog will now end; thanks for ordering!`);
-        } else if(results.response == "no") {
+            //end of results.response == "yes"
+
+        } else if(!results.response) {
+            //rapid closure of dialog on response of 'no'
             session.endDialog(`You said "no", the order has been canceled. This dialog will now end.`);
-        } else {
-            builder.Prompts.text(session, `Please answer with either "yes" or "no".`);
         }
     },
     //end fifth BuyItem function
+
     function (session, results, next) {
-        if(results.response == "yes") {
-            session.endDialog(`You said "yes", order confirmed! This dialog will now end; thanks for ordering!`);
-        } else if(results.response == "no") {
+        let user = session.userData;
+        if(results.response) {
+            if (!user.email && !user.phone) {//no contact information on hand, beginDialog("/ContactInfo");
+            console.log("~~~~~\nline140 hello session.userData:\n", session.userData);
+            console.log("~~~~~\n");
+            session.beginDialog('/ContactInfo');
+
+                // session.beginDialog("/ContactInfo", results);
+                //passing results along, but since the answer will be yes... the results won't (shouldn't) do anything in the /ContactInfo waterfall.
+            } else if (!user.email) {
+                //prompt for email
+                builder.Promtpts.text(session, "What is your email address?");
+            } else if (!user.phone) {
+                //prompt for phone number
+                builder.Prompts.text(session, "What is your phone number?");
+            } else {
+                //user's data exists, user confirms order, dialog ends
+                session.endDialog(`Order confirmed! This dialog will now end; thanks for ordering!`);
+            }
+            //this is the end of results.response == "yes"
+        } else if(!results.response) {
+            //user has decided against the order.
             session.endDialog(`You said "no", the order has been canceled. This dialog will now end.`);
-        } else {
-            session.endDialog(`I'm sorry but I do not understand... I will now end this dialog. Goodbye.`);
         }
-    }
+    },
+    //end sixth BuyItem function
+
+    function (session, results, next) {
+        let user = session.userData;
+
+        console.log("line 170 results.response:\n", results.response);
+        if(!user.email) {
+            let regex = new RegExp(/\w+@+\w+\.+\w{2,10}$/);
+
+            if(regex.test(results.response)) {
+                user.email = results.response;
+                session.endDialog("Email received, order confirmed. Thank you!");
+            }
+        }
+
+        if(!user.phone) {
+            //regex phone number... if(regex.test(results.response)) {
+        //     user.phone = results.response;
+        // } else {
+        //     //go back and ask again.. somehow.
+        // }
+            user.phone = results.reponse;
+            session.endDialog("Phone number received, order confirmed. Thank you!");
+
+        } else {
+            session.endDialog("Contact info received, order conirmed. Thank you!");
+            // next();
+        }
+    },
+    // end seventh BuyItem function
+
+    // function (session, results, next) {
+    //     let user = session.userData;
+    //     if(!user.email) {
+    //         let regex = /[^\s@]+@[^\s@]+\.[^\s@]+/;
+    //         regex.test(results.response);
+    //     } else {
+    //         if(!user.phone) {
+    //             builder.Prompts.text(session, "What is your phone number? Please format it as e.g
+    //\"222-333-4444\".");
+    //         }
+    //         next();
+    //     }
+    //
+    // },
+    //end eighth BuyItem function
+
+    // function (session, results, next) {
+    //     let user = session.userData;
+    //     if(!user.phone) {
+    //         let regex = /[0-9]/;
+    //         let regex = ^[2-9]\d{2}-\d{3}-\d{4}$;
+    //     } else {
+    //         next();
+    //     }
+    //
+    // },
+    //end ninth BuyItem function
+    // function (session, results, next) {
+    //
+    // },
+    // //end eighth BuyItem function
+    // function (session, results, next) {
+    //
+    // },
+    // //end eighth BuyItem function
+    //
+    // function (session, results, next) {
+    //     let user = session.userData;
+    //     let email = builder.EntityRecognizer.findEntity(results.entities,'email');
+    // }
 ])
-//end of intent 'BuyItem'
 
 // dialog.matches('GetShippingAddress', [
 //     function (session, args, next) {
@@ -232,6 +342,7 @@ bot.dialog('/BuyItem', [
 //         }
 //     }
 // ]);
+//end of intent 'BuyItem'
 dialog.matches('ContactInfo', [
     //uses LUIS to match intents...
     function (session, results) {
@@ -241,23 +352,67 @@ dialog.matches('ContactInfo', [
 ]);
 bot.dialog('/ContactInfo', [
     function(session, results, next){
-        if(session.userData) {
-            console.log("\nLine 245, session.userData?\n", session.userData);
+        let user = session.userData;
+        if(user) {
+            console.log("\nLine 363, session.userData?\n", user);
         }
-        let phone = builder.EntityRecognizer.findEntity(results.entities, 'customer.info.phone');
-        let email = builder.EntityRecognizer.findEntity(results.entities, 'builtin.email');
-        session.userData.phone = phone ? phone.entity : null;
-        session.userData.email = email ? email.entity : null;
 
-        console.log("\n~~~~~\nLine 256, results object: ", results);
-        builder.Prompts.text(session, results);
-    },
-    function(session, results, next){
-        console.log("Line 261, session.dialogData:\n", session.userData);
-        if (results) {
-            console.log("\n~~~~~\nLine 261, results object: ", results);
+        if(results) {
+            let phone = builder.EntityRecognizer.findEntity(results.entities, 'customer.info.phone');
+            if(!phone) {
+                phone = builder.EntityRecognizer.findEntity(results.entities, 'builtin.phonenumber');
+            }
+            let email = builder.EntityRecognizer.findEntity(results.entities, 'builtin.email');
+            user.phone = phone ? phone.entity : null;
+            user.email = email ? email.entity : null;
+            console.log("\n~~~~~\nLine 374, results object: ", results);
         }
-        session.endDialog("End of '/ContactInfo'");
+        if(!user.email) {
+            builder.Prompts.text(session, "What is your email address?");
+
+        } else if(!user.phone) {
+            builder.Prompts.text(session, "What is your phone number?");
+
+        } else {
+            next();
+        }
+    },
+
+    function (session,results,next) {
+        let user = session.userData;
+
+        if(!user.email) {
+            let regex = new RegExp(/\w+@+\w+\.+\w{2,10}$/);
+            user.email = regex.test(results.response) ? results.response : null;
+            console.log("\nRegex results:", regex.test(results.response));
+            if (!user.email) {
+                builder.Prompts.text(session, "That is an invalid email, please reenter your email address.");
+            }
+
+            builder.Prompts.text(session, "Line 390, Hello?");
+
+        } else if (!user.phone) {
+            let regex ;
+            user.phone = regex.test(results.response) ? results.response : null;
+            if (user.phone) {
+                builder.Prompts.text(session, "Line 396");
+            }
+        }
+    },
+
+    function(session, results, next){
+        let user = session.userData;
+        console.log("Line 261, session.dialogData:\n", session.userData);
+        // if (results) {
+        //     console.log("\n~~~~~\nLine 261, results object: ", results);
+        // }
+        if (user.email && user.phone) {
+            //email and phone number received, and /ContactInfo ends.
+            session.endDialog("Conact information received, ending of '/ContactInfo'");
+        } else {
+            //if user.email and user.phone are not complete, the dialog restarts.
+            session.beginDialog("/ContactInfo");
+        }
     }
 ])
 dialog.onDefault(builder.DialogAction.send("I'm sorry I didn't understand. I only assist in shopping."));
